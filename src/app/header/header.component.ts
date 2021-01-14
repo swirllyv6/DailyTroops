44b612592972757f9e8a8837132a62419acd4a8c
KadirSheikh <rs7246510@gmail.com>
2021-01-14 14:40:33 +0530

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router'
 })
 export class HeaderComponent implements OnInit {
 
-  @Input() page: string;
+  @Input() pageName: string;
 
   constructor(private location: Location , private router:Router) { 
     
