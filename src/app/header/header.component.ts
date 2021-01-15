51d5824c544e8265ae8fc9f61efd0cc3635e7b09
KadirSheikh <rs7246510@gmail.com>
2021-01-15 19:52:05 +0530

    modify checkout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router'
 })
 export class HeaderComponent implements OnInit {
 
-  @Input() pageName: string;
+  @Input() page: string;
 
   constructor(private location: Location , private router:Router) { 
     
