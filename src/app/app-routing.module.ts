6ac85f861311b5b4875f7230f03ff2fb8388460e
KadirSheikh <rs7246510@gmail.com>
2021-01-17 16:48:52 +0530

    added order details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrderHistoryDetailsComponent } from './order-history/order-history-details/order-history-details.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ServiceCheckoutComponent } from './services/service-checkout/service-checkout.component';
@@ -29,6 +30,7 @@ const routes: Routes = [
   },
   { path: 'servicedetail/:serviceId/:garageId', component: ServiceDetailComponent },
   { path: 'orderhistory', component: OrderHistoryComponent },
+  { path: 'orderdetail', component: OrderHistoryDetailsComponent },
   { path: 'servicepayment', component:ServicePaymentComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
